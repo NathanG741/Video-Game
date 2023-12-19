4ad2dc2086be6cb0d76e44b1b937ca00fce0b726
nathan <nathan.guardado@yahoo.com>
2023-12-19 07:56:57 -0500

    Added more animation images. Started cutting out subImages of the animations.

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -2,20 +2,27 @@ package Main;
 
 import Inputs.KeyboardInputs;
 import Inputs.MouseInputs;
+
+import javax.imageio.ImageIO;
 import javax.swing.*; //JFrame
 import java.awt.*; //Graphics
-
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
     private float xDelta = 100, yDelta = 100;
+    private BufferedImage img, subImg;
 
 
 
 
 
     public GamePanel() {
+
+        importImg();
 
         mouseInputs = new MouseInputs(this);
         setPanelSize();
@@ -25,8 +32,24 @@ public class GamePanel extends JPanel {
 
     }
 
+    private void importImg() {
+        InputStream is = getClass().getResourceAsStream("/Idle.png");
+
+        try {
+            img = ImageIO.read(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     private void setPanelSize() {
-        Dimension size = new Dimension(1280,800);
+        Dimension size = new Dimension(1440,800);
         setMinimumSize(size);
         setPreferredSize(size);
         setMaximumSize(size);
@@ -50,8 +73,8 @@ public class GamePanel extends JPanel {
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-        g.drawImage(null, x, y, null);
+        subImg = img.getSubimage(54, 48, 26, 38);
+        g.drawImage(subImg, (int)xDelta, (int)yDelta, 78, 116, null);
 
 
 
