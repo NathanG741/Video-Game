673dbfabf34407dbcfbdfb8a84a348bc52728755
NathanG741 <nathan.guardado@yahoo.com>
2024-01-01 22:57:39 -0500

    Goofin

--- test/Inputs/KeyboardInputsTest.java
+++ test/Inputs/KeyboardInputsTest.java
@@ -11,7 +11,38 @@ import static org.junit.jupiter.api.Assertions.*;
 class KeyboardInputsTest {
 
     @Test
-    void keyReleased() {
+    void keyReleased() throws InterruptedException, AWTException {
+        Thread.sleep(1000);
+        Robot robot = new Robot();
+        robot.keyPress(KeyEvent.VK_H);
+        robot.keyPress(KeyEvent.VK_I);
+
+        robot.keyPress(KeyEvent.VK_SPACE);
+
+        robot.keyPress(KeyEvent.VK_T);
+        robot.keyPress(KeyEvent.VK_H);
+        robot.keyPress(KeyEvent.VK_I);
+        robot.keyPress(KeyEvent.VK_S);
+
+        robot.keyPress(KeyEvent.VK_SPACE);
+
+        robot.keyPress(KeyEvent.VK_I);
+        robot.keyPress(KeyEvent.VK_S);
+
+        robot.keyPress(KeyEvent.VK_SPACE);
+
+        robot.keyPress(KeyEvent.VK_A);
+
+
+        robot.keyPress(KeyEvent.VK_SPACE);
+
+        robot.keyPress(KeyEvent.VK_T);
+        robot.keyPress(KeyEvent.VK_E);
+        robot.keyPress(KeyEvent.VK_S);
+        robot.keyPress(KeyEvent.VK_T);
+
+        robot.keyPress(KeyEvent.VK_ENTER);
+
     }
 
     // :( doesnt work
