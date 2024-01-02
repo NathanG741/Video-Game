0622925bc9ce0e3544eb438dec262761d4b10514
NathanG741 <nathan.guardado@yahoo.com>
2024-01-01 22:49:04 -0500

    Tests 4 (in progress)

--- test/Inputs/KeyboardInputsTest.java
+++ test/Inputs/KeyboardInputsTest.java
@@ -1,6 +1,10 @@
 package Inputs;
 
+import Main.GamePanel;
 import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,7 +14,15 @@ class KeyboardInputsTest {
     void keyReleased() {
     }
 
-    @Test
-    void keyPressed() {
-    }
+    // :( doesnt work
+//    @Test
+//    void keyPressed() throws AWTException {
+//        GamePanel gamePanel = new GamePanel();
+//        KeyboardInputs keyboardInputs = new KeyboardInputs(gamePanel);
+//
+//        Robot robot = new Robot();
+//        robot.keyPress(KeyEvent.VK_W);
+//
+//        assertEquals(1, gamePanel.getDirection());
+//    }
 }
